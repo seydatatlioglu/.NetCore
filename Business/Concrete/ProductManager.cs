4c8226fdf7fa152434ca5e11405c4557eb2fb149
seydatatlioglu <statlioglu0@gmail.com>
2021-08-10 12:49:18 +0300

    result yapısı

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Core.Utilities.Results;
 using DataAccess.Abstarct;
 using DataAccess.Concrete.InMemory;
@@ -23,40 +24,46 @@ namespace Business.Concrete
         {
             if (product.ProductName.Length<2)
             {
-                return new ErrorResult("ürün ismi min 2 karakter olmalı");
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
              _productDal.Add(product);
-            return new SuccessResult("Ürün eklendi");
+            return new SuccessResult(Messages.ProductNameInvalid);
         }
 
-        public List<Product> GetAll()
+        public IDataResult< List<Product>> GetAll()
         {
-            //iş kodları
-            //yetkisi varsa
-            return _productDal.GetAll(); 
+            if (DateTime.Now.Hour==22)
+            {
+                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(),Messages.ProductListed); 
 
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
-            return _productDal.GetAll(p => p.CategoryId == id);
+            return new SuccessDataResult<List<Product>>( _productDal.GetAll(p => p.CategoryId == id));
         }
 
   
 
-        public List<Product> GetAllByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetAllByUnitPrice(decimal min, decimal max)
         {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
         }
 
-        public Product GetById(int productId)
+        public IDataResult<Product> GetById(int productId)
         {
-            return _productDal.Get(p => p.ProductId == productId);
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
-            return _productDal.GetProductDetails();
+            if (DateTime.Now.Hour == 12)
+            {
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
+            }
+            return new SuccessDataResult<List<ProductDetailDto>> (_productDal.GetProductDetails());
         }
 
        
---
+++ Core/Utilities/Results/IDataResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public interface IDataResult<T>:IResult //içerisinde hem işlem hem mesaj hemde data vericek.. IResult içerisinde de hem mesaj hemde işlem sonucu var (DRY)
+    {
+        T Data { get; } //data için sonucu (result'ı) burada get ederiz..
+    }
+}
